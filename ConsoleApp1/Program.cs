17b643589001f2d2b5b7edf60134e3d92a92f748
Horta <Horta@DESKTOP-DQM7GVE>
2022-12-04 17:34:16 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Миронова Анастасия 22-ИСП-2/1
+//Лаб 5.2 Вариант 4 
+//уроевень база
+
+
+using MyClassLibrary1;
+using System;
+
+
+namespace ConsoleApp1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+           Class1.Lib();
+        }
+    }
+}
---
+++ MyClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyClassLibrary1
+{
+    public class Class1
+    {
+        static public void Lib()
+        {
+            int[,] mas = new int[3, 6];
+            Random rnd = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    mas[i, j] = rnd.Next(0, 100);
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
